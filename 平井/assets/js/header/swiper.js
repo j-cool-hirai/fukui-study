b6ddf54639d043c99169d300c493b221bb958bff
j-cool-hirai <hirai@j-cool.fm>
2023-08-25 15:55:49 +0900

    メニュー途中まで

--- 平井/assets/js/header/swiper.js
+++ 平井/assets/js/header/swiper.js
@@ -1,17 +1,15 @@
 const swiper = new Swiper(".swiper", {
     loop: true,
-    // 画像の枚数
     slidesPerView: 4,
-    // 画像の間隔
     spaceBetween: 20,
-    // ページネーション
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      type: "bullets", // ページネーションの種類を指定
     },
     // 前後の矢印
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
