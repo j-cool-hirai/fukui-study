d51fc82d1bf31df6459920faa707fba75136dd05
j-cool-hirai <hirai@j-cool.fm>
2023-08-28 10:31:37 +0900

    途中まで

--- 平井/assets/js/header/swiper.js
+++ 平井/assets/js/header/swiper.js
@@ -1,15 +1,30 @@
-const swiper = new Swiper(".swiper", {
-    // loop: true,
-    slidesPerView: 4,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      type: "bullets", // ページネーションの種類を指定
-    },
-    // 前後の矢印
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+// const swiper = new Swiper(".swiper", {
+//     slidesPerView: 4,
+//     spaceBetween: 20,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       type: "bullets", // ページネーションの種類を指定
+//     },
+//     // 前後の矢印
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+// });
+
+// .myswiper-wrapperを持つ要素を全て取得
+const sampleSwiper = document.querySelectorAll(".p-sample__menu-swiper");
+console.log(sampleSwiper);
+
+sampleSwiper.each(function (index, el) {
+    const swiper = new Swiper(el.querySelector(".swiper"), {
+      pagination: {
+        el: el.querySelector(".swiper-pagination")
+      },
+      navigation: {
+        nextEl: el.querySelector(".swiper-button-next"),
+        prevEl: el.querySelector(".swiper-button-prev")
+      }
+    });
 });
