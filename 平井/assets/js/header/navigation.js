ecb0fa1fae98feac9eed89d6b116676419cf4783
j-cool-hirai <hirai@j-cool.fm>
2023-08-30 17:58:32 +0900

    スクロール固定処理追加

--- 平井/assets/js/header/navigation.js
+++ 平井/assets/js/header/navigation.js
@@ -6,4 +6,31 @@ headerButton.addEventListener('click', () => {
     headerButton.classList.toggle('js-sample__header-button-active');
     // .p-sample__header-navにjs-sample__header-nav-activeクラスを付与する
     document.querySelector('.p-sample__header-nav').classList.toggle('js-sample__header-nav-active');
+
+        // 現在のスクロール量を取得
+    const scrollY = window.scrollY;
+    // bodyタグにu-fixedクラスを付与する
+    document.body.classList.toggle("u-fixed");
+
+        // bodyタグに.u-fixedが付与されている場合
+    if (document.body.classList.contains("u-fixed")) {
+      // 現在のブラウザの横幅を取得
+      const width = window.innerWidth;
+      // bodyタグにstyleでwidthの値を設定する
+      document.body.style.width = width + "px";
+      // bodyタグにstyleでtopの値を設定する
+      document.body.style.top = "-" + scrollY + "px";
+    } else {
+      // 保存したスクロール量を取得
+      const scrollY_Retention = Math.abs(
+        parseInt(document.body.style.top)
+      );
+
+      // bodyタグにstyleでwidthの値を削除する
+      document.body.style.width = "";
+      // bodyタグにstyleでtopの値を削除する
+      document.body.style.top = "";
+      // 現在のスクロール量を設定する
+      window.scrollTo(0, scrollY_Retention);
+    }
 });
